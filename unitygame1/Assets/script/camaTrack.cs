01153a8a03eb7f480e075ab42fb0a70712749650
yiliu1203 <yiliu1203@163.com>
2015-06-01 23:39:29 +0800

    0601
    0601

---
+++ unitygame1/Assets/script/camaTrack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camaTrack : MonoBehaviour {
+	public Transform [] transobj;
+	private Animator [] animator;
+	public float speed=10;
+	// Use this for initialization
+	void Start () {
+		animator = new Animator[transobj.Length];
+		for (int i=0; i<transobj.Length; i++) {
+			animator[i]=transobj[i].GetComponent<Animator>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float tranX = Input.GetAxis ("Vertical") * speed;
+		float tranZ = Input.GetAxis ("Horizontal") * speed;
+		tranX *= Time.deltaTime;
+		tranZ *= Time.deltaTime;
+		for (int i=0; i<transobj.Length; i++) {
+			transobj[i].transform.Translate(new Vector3(tranX,0,tranZ));
+		}
+	
+	}
+}
