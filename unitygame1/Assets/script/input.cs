d1f986317a279ece2d88605b1e71ae8fc8d727a6
yiliu1203 <yiliu1203@163.com>
2015-06-04 21:56:28 +0800

    0604-3

--- unitygame1/Assets/script/input.cs
+++ unitygame1/Assets/script/input.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public class input : MonoBehaviour {
 
 	// Use this for initialization
+
 	void Start () {
-	
+		animation.wrapMode = WrapMode.Once;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if (Input.GetAxis ("Vertical") > 0.2) {
+			animation.CrossFade("walk");
+		}
+		if (Input.GetAxis ("Vertical") < -0.2) {
+			animation.Stop();
+		}
 	}
 }
